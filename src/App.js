9182c75be2049ff69a0d0d2f4ae4edb11ec22e46
dineshsadaram <dineshsadaram143@gmail.com>
2025-04-27 02:50:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Veda Transit</h1>
+      <p>Transport Tracking and Scheduling Portal</p>
+    </div>
+  );
+}
+
+export default App;
