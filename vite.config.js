1dbe66d12d63d56e9aea30b3cafffab0565fb8b8
DINESH <dinesh@DINESHs-MacBook-Air.local>
2025-04-27 03:25:48 +0530

    First upload of Veda Transit Frontend

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
 });
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+>>>>>>> 2c3288a (First upload of Veda Transit Frontend)
