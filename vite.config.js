33a685843a7a2e6955ad1b12a6bbb872203e9709
dineshsadaram <dineshsadaram143@gmail.com>
2025-04-27 04:39:12 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> 2c3288a (First upload of Veda Transit Frontend)
